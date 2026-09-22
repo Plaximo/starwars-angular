5fe58abceb8aa600643dec3757ef16daed20d32f
Max Grünwald <max.gruenwald@gmail.com>
2026-09-22 10:55:56 +0200

    add styles and filterable people

--- src/app/features/people/services/people-viewmodel.service.ts
+++ src/app/features/people/services/people-viewmodel.service.ts
@@ -6,15 +6,18 @@ import { People } from '../../../core/models';
 
 @Injectable({ providedIn: 'root' })
 export class PeopleViewmodel {
-  // TODO add viewmodel class
-  // can be swapped to a MockRepository or a different api can be integrated
   private peopleRepository: IPeopleRepository = inject(SwapiPeopleRepository);
 
   readonly search = signal('');
   readonly people = toSignal(this.peopleRepository.getAll(), { initialValue: [] });
+  readonly isLoading = computed(() => this.people().length === 0);
 
   readonly filteredPeople = computed(() => {
-    const query = this.search().toLowerCase();
+    const query = this.search().toLowerCase().trim();
     return this.people().filter((p: People) => p.name.toLowerCase().includes(query));
   });
+
+  setSearch(query: string): void {
+    this.search.set(query);
+  }
 }
